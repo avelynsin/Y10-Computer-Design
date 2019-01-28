3c00a5f9ee0a3ceb4568d3fe5f0032f4dbdbfdb1
avelynsin <46880429+avelynsin@users.noreply.github.com>
2019-01-28 12:59:15 +0800

    Add files via upload

--- p5jsTutorial1.3-master/sketch.js
+++ p5jsTutorial1.3-master/sketch.js
@@ -1,7 +1,7 @@
 function setup()
 {
   createCanvas(700,500);
-  
+
 }
 
 function draw()
@@ -12,16 +12,16 @@ function draw()
 	stroke(0,0,0);
 	strokeWeight(3);
 
-	text("Hello World",80,30);
-
 	rect(10,10,100,50);
+
+  text("Hello World",80,30);
 
 	text("Baby Shark",10,50);
 
 	rect(10,100,100,50);
 
 	fill(255,255,255);
-	
+
 	text("Donald Trump",10,130);
 
 }
