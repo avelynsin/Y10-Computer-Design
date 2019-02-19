37d121222f5822fe10608f785284bc1a93cf3cd4
avelynsin <46880429+avelynsin@users.noreply.github.com>
2019-02-19 23:28:34 +0800

    Add files via upload

--- p5jsTutorial2.1-master/sketch.js
+++ p5jsTutorial2.1-master/sketch.js
@@ -1,13 +1,14 @@
 var button2X;
 var button2Y;
+var button2Z;
 
 function setup()
 {
   createCanvas(700,500);
   button2X = 10;
   button2Y = 150;
+  button2Z = 290;
 
-  
 }
 
 function draw()
@@ -20,14 +21,14 @@ function draw()
 		fill(255,200,0);
 		stroke(0,0,0);
 		strokeWeight(3);
-		rect(10,10,100,50);		
+		rect(10,10,100,50);
 	}
 	else
 	{
 		fill(200,200,255);
 		stroke(0,0,0);
 		strokeWeight(3);
-		rect(10,10,100,50);		
+		rect(10,10,100,50);
 	}
 
 	fill(255,255,255);
@@ -56,5 +57,32 @@ function draw()
 		textSize(12);
 		text("Options",button2X+30,button2Y+30);
 	}
+
+// added
+    fill(250,250,250);
+    stroke(0,0,0);
+    rect(button2X,button2Z,100,50);
+
+    if (mouseX > 10 && mouseX < 10 + 100 && mouseZ > 10 && mouseZ < 10+100)
+	{
+	    // Change button color when we move our mouse over it
+		fill(100,50,5);
+		stroke(51,70,190);
+		textSize(16)
+		rect(button2X,button2Z,100,50);
+  }
+  	else
+  	{
+  		// Otherwise show the original color
+      fill(250,250,250);
+      stroke(0,0,0);
+      textSize(12)
+      rect(button2X,button2Z,100,50);
+  	}
+
+    fill(140,100,205);
+    stroke(0,0,0);
+    textSize(12);
+    text("Avelyn Sin",30,320);
 
 }
