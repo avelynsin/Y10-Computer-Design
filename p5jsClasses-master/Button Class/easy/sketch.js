5446111dfb6bbcfa228945c1148d7c71f8596d89
avelynsin <46880429+avelynsin@users.noreply.github.com>
2019-03-13 11:27:29 +0800

    Add files via upload

--- p5jsClasses-master/Button Class/easy/sketch.js
+++ p5jsClasses-master/Button Class/easy/sketch.js
@@ -3,7 +3,7 @@ var b1;
 function setup()
 {
 	createCanvas(500,500);
-	b1 = new Button(10,10,100,25);
+	b1 = new Button(10,150,100,25);
 	b1.setText("level1");
 	b1.setTextOver("GO");
 	b1.setButtonOverFill(255,255,50);
